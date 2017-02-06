9dd68bcd1cb8db72053279394f6b871203bf1dec
Adam Melaku <AdamMelaku362@gmail.com>
2017-02-06 13:27:23 -0600

    test commit

---
+++ Adam_Melaku/TryPython.py
@@ -0,0 +1,10 @@
+Test_variable=22
+Test_Variable=25
+
+print Test_Variable*4
+
+Str1 = "Test"
+Str2 = "String"
+
+print Str1+Str2
+print Str1+str(Test_Variable)
---
+++ Adam_Melaku/example.py
@@ -0,0 +1,10 @@
+test = 4
+test = 25
+
+print test*4
+
+str1 = "Coding"
+str2 = "Dojo"
+
+print str1+str2
+print str1+str(test)
